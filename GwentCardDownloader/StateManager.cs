09135da756017d179aa7b49f174533678f87ec41
JustAGhosT <smit.jurie@gmail.com>
2025-01-27 02:42:25 +0200

    Create `StateManager` class to manage card download state
    * Add private readonly string `_statePath` and `ConcurrentDictionary<string, CardDownloadState>` field `_state`
    * Implement async method `SaveStateAsync` to serialize `_state` to JSON and write to `_statePath`
    * Implement async method `LoadStateAsync` to read JSON from `_statePath` and deserialize to `Dictionary<string, CardDownloadState>`

---
+++ GwentCardDownloader/StateManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GwentCardDownloader
+{
+    public class StateManager
+    {
+        private readonly string _statePath;
+        private readonly ConcurrentDictionary<string, CardDownloadState> _state;
+
+        public StateManager(string statePath)
+        {
+            _statePath = statePath;
+            _state = new ConcurrentDictionary<string, CardDownloadState>();
+        }
+
+        public async Task SaveStateAsync()
+        {
+            var json = JsonSerializer.Serialize(_state, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            await File.WriteAllTextAsync(_statePath, json);
+        }
+
+        public async Task<Dictionary<string, CardDownloadState>> LoadStateAsync()
+        {
+            if (!File.Exists(_statePath))
+                return new Dictionary<string, CardDownloadState>();
+
+            var json = await File.ReadAllTextAsync(_statePath);
+            return JsonSerializer.Deserialize<Dictionary<string, CardDownloadState>>(json);
+        }
+    }
+
+    public class CardDownloadState
+    {
+        public string CardId { get; set; }
+        public bool IsDownloaded { get; set; }
+        public int RetryCount { get; set; }
+    }
+}
